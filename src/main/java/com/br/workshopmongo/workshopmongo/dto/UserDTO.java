2b419895ac0065174b131a18c8cba9e77b9a032f
almeidaluan <almeida.luan099@gmail.com>
2019-02-26 14:42:41 -0300

    Utilizando DTO para trafegar os dados

---
+++ src/main/java/com/br/workshopmongo/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.br.workshopmongo.workshopmongo.dto;
+
+import com.br.workshopmongo.workshopmongo.domain.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class UserDTO implements Serializable {
+
+
+    private static final long serialVersionUID = 1l;
+
+    private String id;
+    private String name;
+    private String email;
+
+
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+}
